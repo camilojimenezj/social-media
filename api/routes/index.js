6d79bfcdf2a7d0d5e728ca23a52ea5bd1ee93c19
Camilo Jimenez <camilojj14@gmail.com>
2022-09-05 15:08:52 -0500

    feat: add login route and JWT

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const userRouter = require('./users')
 const postRouter = require('./posts')
+const loginRouter = require('./login')
 const notFound = require('../middlewares/notFound')
 const errorHandler = require('../middlewares/errorHandler')
 
@@ -9,6 +10,9 @@ router.use('/api/users', userRouter)
 
 // Posts
 router.use('/api/posts', postRouter)
+
+// Login
+router.use('/api/login', loginRouter)
 
 // Route not found
 router.use(notFound)
